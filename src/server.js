fdce625056ba8d9badb1df42bbc027b7f71615f8
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-09 12:44:58 -0300

    routes set up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/routes.js';
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.json());
+app.use(router)
 
 const PORT = 3000;
 app.listen(PORT, () => {
