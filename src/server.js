ce3ff70607d87bc918419323de6cefeff3e7ab12
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-10 13:59:34 -0300

    cors and https set up

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import router from './routes/routes.js';
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(router)
+app.use(router);
+app.use(cors());
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+https.createServer({
+  key: fs.readFileSync('src/SSL/code.key'),
+  cert: fs.readFileSync('src/SSL/code.crt'),
+// Usar a porta 443 em produção
+}, app).listen(443, () => console.log('Server listening on port 443'));
+// }, app).listen(3001, () => console.log('Server listening on port 3001'));
